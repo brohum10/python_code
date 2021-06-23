c80b2b8cb60946b706b749fe615fbc0c04cfc74e
brohum10 <80368266+brohum10@users.noreply.github.com>
2021-06-23 16:02:09 -0700

    Add files via upload

---
+++ daniel_liang/chapter04/4.16.py
@@ -0,0 +1,61 @@
+import random
+
+random_num = random.randint(1, 24)
+
+a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+ 
+
+if random_num == 1:
+    letter = a
+elif random_num == 2:
+    letter = b
+elif random_num == 3:
+    letter = c
+elif random_num == 4:
+    letter = d
+elif random_num == 5:
+    letter = e
+elif random_num == 6:
+    letter = f
+elif random_num == 7:
+    letter = g
+elif random_num == 8:
+    letter = h
+elif random_num == 9:
+    letter = i
+elif random_num == 10:
+    letter = j
+elif random_num == 11:
+    letter = k
+elif random_num == 12:
+    letter = l
+elif random_num == 13:
+    letter = m
+elif random_num == 14:
+    letter = n
+elif random_num == 15:
+    letter = o
+elif random_num == 16:
+    letter = p
+elif random_num == 17:
+    letter = q
+elif random_num == 18:
+    letter = r
+elif random_num == 19:
+    letter = s
+elif random_num == 20:
+    letter = t
+elif random_num == 21:
+    letter = u
+elif random_num == 22:
+    letter = v
+elif random_num == 23:
+    letter = w
+elif random_num == 24:
+    letter = x
+elif random_num == 25:
+    letter = y
+elif random_num == 26:
+    letter = z
+
+print(letter)

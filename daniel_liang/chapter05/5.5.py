1912b120d4083583d3be5fcdc47e5bea3d35d4d0
brohum10 <80368266+brohum10@users.noreply.github.com>
2021-06-25 12:22:41 -0700

    Add files via upload

---
+++ daniel_liang/chapter05/5.5.py
@@ -0,0 +1,10 @@
+kilogram = 1
+pound = 20
+print("Kilorgams \t Pounds \t | \t Pounds \t Kilorgams")
+
+while (kilogram in range(1, 200)) or (pound in range(20, 520)):
+    pounds = kilogram * 2.2
+    kilograms = pound / 2.2
+    print(str(format(kilogram, "<4.0f")) + str(format(pounds, "20.1f")) + " \t |"+ str(format(pound, "20.0f")) + str(format(kilograms, "20.2f")))
+    kilogram+=2
+    pound+=5

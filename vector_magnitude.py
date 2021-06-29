c239edda8ef215a49af8f17acb3e491ea360e68c
brohum10 <80368266+brohum10@users.noreply.github.com>
2021-06-29 12:24:14 -0700

    Delete vector_magnitude.py

--- vector_magnitude.py
+++
@@ -1,7 +0,0 @@
-import math
-
-x1, y1 = eval(input("Enter first x and y coordinates to calculate vector distace : "))
-x2, y2 = eval(input("Enter second x and y coordinates to calculate vector distace : "))
-
-d = math.sqrt(((x2 - x1)**2) + ((y2 - y1)**2))
-print(d)
